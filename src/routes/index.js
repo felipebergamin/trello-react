9c39e0f31db43a9fbc686a0a8e95d8959f22a3cc
Felipe Bergamin <felipebergamin6@gmail.com>
2020-04-26 18:15:04 -0300

    add routing with blank login page

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import Login from '../pages/Login';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact render={() => <Redirect to="/login" />} />
+        <Route path="/login" component={Login} exact />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
