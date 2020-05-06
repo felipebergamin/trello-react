93f27b5320d29a8a1ccd05ba0de1fd94c52e3899
Felipe Bergamin <felipebergamin6@gmail.com>
2020-05-06 01:30:25 -0300

    rename default layout to auth layout

--- src/pages/_layouts/auth/index.js
+++ src/pages/_layouts/auth/index.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 
 import { Wrapper } from './styles';
 
-export default function DefaultLayout({ children }) {
+export default function AuthLayout({ children }) {
   return <Wrapper>{children}</Wrapper>;
 }
 
-DefaultLayout.propTypes = {
+AuthLayout.propTypes = {
   children: PropTypes.node.isRequired,
 };
---
+++ src/pages/_layouts/auth/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  background-color: #f9fafc;
+
+  flex: 1;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
