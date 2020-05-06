9871a231ebfe4b6b0511e2d4a4d5a36386f91f4a
Felipe Bergamin <felipebergamin6@gmail.com>
2020-05-05 23:41:41 -0300

    add firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyDuQ0Nl1lUJq0sXLArAUfwxg7xbciVoEGQ',
+  authDomain: 'frello-9a86b.firebaseapp.com',
+  databaseURL: 'https://frello-9a86b.firebaseio.com',
+  projectId: 'frello-9a86b',
+  storageBucket: 'frello-9a86b.appspot.com',
+  messagingSenderId: '321498117192',
+  appId: '1:321498117192:web:c39f7ed138f04aa375147d',
+  measurementId: 'G-L3QVW0BZKF',
+};
