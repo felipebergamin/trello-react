de3a02b440d02c0cf4d81e5b899a3b0178cab24b
Felipe Bergamin <felipebergamin6@gmail.com>
2020-05-12 22:36:09 -0300

    add auth listener

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import firebase from 'firebase/app';
 
 export const AuthContext = React.createContext({});
 AuthContext.displayName = 'AuthContext';
 
 export function AuthProvider({ children }) {
   const [isSigned, setSigned] = React.useState(false);
+
+  React.useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setSigned(true);
+      } else {
+        setSigned(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   const signIn = () => {
     setSigned(true);
